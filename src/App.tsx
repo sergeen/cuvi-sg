955e4f997dd22064c01ae771f468633c8ad015c7
Sergio Scotta <sergeen@gmail.com>
2024-05-27 00:07:46 -0300

    Remove code add some basic elements
    I'm going very slow, intentionally. while reading about accessible
    web I do some sketches and from there a basic html structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      
+      <nav>
+        <ul>
+          <li>Accesibilidad</li>
+          <li>Idioma</li>
+        </ul>
+      </nav>
+
+      <header>
+        <h1>About me</h1>
+        <img src="" alt="" />
+        <div>
+          <img src="" alt="" />
+          <p>Sergio Scotta</p>
+          <p>I´m a UX Developer</p>
+        </div>
+        <p>
+          I present myself as an emotionally intelligent, technically proficient, and communicative front-end developer with experience in UX and UI design for small startups and large organizations.
+        </p>
+        <button>versiones descargables</button>
+      </header>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
